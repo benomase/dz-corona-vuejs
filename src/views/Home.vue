7cec89f37977904424fde19c61f745a03d4c86da
seif <seif.benomar@cabri.com>
2020-03-17 10:21:00 +0100

    Added more buttons and side nav

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,20 @@
 <template>
-   <!-- <hello-world />-->
+  <!-- <hello-world />-->
 
   <v-container fluid class="fill-height transparent">
-    <v-btn color="info" fixed right top @click="go('newStudent')" class="pointer-events">
+    <v-btn color="info" fixed right top class="pointer-events"
+           @click.stop="drawer = !drawer">
       <v-icon dark>more</v-icon>
     </v-btn>
-    <v-btn color="info" fixed left bottom @click="go('newStudent')" class="pointer-events">
+    <v-btn color="info" fixed left bottom class="pointer-events">
       <v-icon dark>add</v-icon>
     </v-btn>
-
 
     <v-speed-dial
       v-model="fab"
       fixed
       right
       bottom
-      :direction="direction"
-      :open-on-hover="hover"
-      :transition="transition"
     >
       <template v-slot:activator>
         <v-btn
@@ -59,20 +56,124 @@
         <v-icon>mdi-delete</v-icon>
       </v-btn>
     </v-speed-dial>
+
+    <v-dialog
+      v-model="dialogTOS"
+      max-width="290"
+    >
+      <v-card>
+        <v-card-title class="headline">Use Google's location service?</v-card-title>
+
+        <v-card-text>
+          Let Google help apps determine location. This means sending anonymous location data to Google, even when no
+          apps are running.
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn
+            color="green darken-1"
+            text
+            @click="dialogTOS = false"
+          >
+            Disagree
+          </v-btn>
+
+          <v-btn
+            color="green darken-1"
+            text
+            @click="dialogTOS = false"
+          >
+            Agree
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog
+      v-model="dialogCase"
+      max-width="290"
+    >
+      <v-card>
+        <v-card-title class="headline">Use Google's location service?</v-card-title>
+
+        <v-card-text>
+          Let Google help apps determine location. This means sending anonymous location data to Google, even when no
+          apps are running.
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <div class="text-center">
+            <v-btn class="ma-2" center tile large color="teal" icon>
+              <v-icon>share</v-icon>
+            </v-btn>
+          </div>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>John Leider</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
   </v-container>
 
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+    import HelloWorld from '../components/HelloWorld'
 
-  export default {
-    name: 'Home',
+    export default {
+        name: 'Home',
 
-    components: {
-      HelloWorld,
-    },
-  }
+        components: {
+            HelloWorld,
+        },
+        data() {
+            return {
+                dialogTOS: true,
+                dialogCase: false,
+                drawer: null,
+                fab: null,
+                items: [
+                    {title: 'Home', icon: 'dashboard'},
+                    {title: 'About', icon: 'question_answer'},
+                ],
+            }
+        }
+    }
 </script>
 
 <style>
